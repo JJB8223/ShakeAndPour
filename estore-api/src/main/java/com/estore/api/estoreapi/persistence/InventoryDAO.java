0e9dab46bef433818ef2fc13716cdc305bf5dcca
Matthew Morrison <msm8275@rit.edu>
2024-02-15 17:00:27 -0500

    Added function for creating a product, implementation in InventoryFileDAO similar to functionality in ProductFileDAO (needs to be updated)

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDAO.java
@@ -1,10 +1,25 @@
 package com.estore.api.estoreapi.persistence;
 
+import com.estore.api.estoreapi.model.Product;
+
+import java.io.IOException;
+
 /**
  * Defines the interface for Inventory object persistance
  *
  * @author Matthew Morrison msm8275
  */
 public interface InventoryDAO {
-
+    /**
+     * Creates and saves a {@linkplain Product Product}
+     *
+     * @param Product {@linkplain Product Product} object to be created and saved
+     * <br>
+     * The id of the Product object is ignored and a new uniqe id is assigned
+     *
+     * @return new {@link Product Product} if successful, false otherwise
+     *
+     * @throws IOException if an issue with underlying storage
+     */
+    Product createProduct(Product Product) throws IOException;
 }
