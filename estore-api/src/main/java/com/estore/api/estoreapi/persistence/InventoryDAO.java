7e6b8b1ce85ea334fd95fccef160c7bfe736a8be
Matthew Morrison <msm8275@rit.edu>
2024-02-15 16:38:06 -0500

    Initial creater of InventoryFileDAO class and InventoryDAO interface

---
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryDAO.java
@@ -0,0 +1,10 @@
+package com.estore.api.estoreapi.persistence;
+
+/**
+ * Defines the interface for Inventory object persistance
+ *
+ * @author Matthew Morrison msm8275
+ */
+public interface InventoryDAO {
+
+}
---
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/InventoryFileDAO.java
@@ -0,0 +1,12 @@
+package com.estore.api.estoreapi.persistence;
+
+import java.util.logging.Logger;
+
+/**
+ * Implements the functionality for JSON file-based persistence for Inventory
+ *
+ * @author Matthew Morrison
+ */
+public class InventoryFileDAO  implements InventoryDAO{
+    private static final Logger LOG = Logger.getLogger(InventoryFileDAO.class.getName());
+}
