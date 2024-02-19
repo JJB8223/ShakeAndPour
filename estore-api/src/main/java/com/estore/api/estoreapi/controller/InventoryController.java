c3ee038138147d88be1851039316fe1667616401
Akhil Devarapalli <devarapalli148@gmail.com>
2024-02-19 00:22:33 -0500

    Added the api route to search the inventory by a product name

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/InventoryController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/InventoryController.java
@@ -5,9 +5,11 @@ import com.estore.api.estoreapi.model.Product;
 import com.estore.api.estoreapi.persistence.InventoryDAO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -31,6 +33,32 @@ public class InventoryController{
         this.inventoryDao = inventoryDao;
     }
 
+
+    /**
+     * Responds to the GET request for all {@linkplain Product products} whose name contains
+     * the text in name
+     * 
+     * @param name The name parameter which contains the text used to find the {@link Product products}
+     * 
+     * @return ResponseEntity with array of {@link Product Product} objects (may be empty) and
+     * HTTP status of OK<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     * <p>
+     * Example: Find all products that contain the text "ma"
+     * GET http://localhost:8080/products/?name=ma
+     * @throws IOException 
+     */
+    @GetMapping("/")
+    public ResponseEntity<Product[]> searchProducts(@RequestParam String name) throws IOException {
+        LOG.info("GET /products/?name="+name);
+        try {
+            Product[] products = inventoryDao.findProducts(name);
+            return new ResponseEntity<>(products, HttpStatus.OK);
+        } catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
     /**
      * Creates a {@linkplain Product Product} with the provided Product object
