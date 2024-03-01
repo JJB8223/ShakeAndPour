5dd7edafcc0ac23a1c0338fd41a6793861e6cd04
JJB8223 <jjb8223@g.rit.edu>
2024-02-29 21:26:39 -0500

    added update test, fixed create and delete coverage

--- estore-api/src/test/java/com/estore/api/estoreapi/persistence/InventoryFileDAOTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/persistence/InventoryFileDAOTest.java
@@ -68,19 +68,24 @@ public class InventoryFileDAOTest {
 
         p = inventoryFileDAO.getProduct(100);
 
+        assertNull(inventoryFileDAO.getProduct(88));
+
         assertEquals(p, testInventory[1]);
     }
 
     @Test
-    public void testDeleteProd(){
+    public void testDeleteProduct(){
         boolean r = assertDoesNotThrow(() -> inventoryFileDAO.deleteProduct(99),
                 "Unexpected exception thrown");
+        boolean fail = assertDoesNotThrow(() -> inventoryFileDAO.deleteProduct(88),
+                "Unexpected exception thrown");
+        assertFalse(fail);
         assertTrue(r);
         assertEquals(inventoryFileDAO.inventory.size(), testInventory.length-1);
     }
 
     @Test
-    public void testCreateProd(){
+    public void testCreateProduct(){
         Product p = new Product(102, "Sparkling Water", 3.00F, 10);
         Product r = assertDoesNotThrow(() -> inventoryFileDAO.createProduct(p),
                 "Unexpected exception thrown");
@@ -94,5 +99,26 @@ public class InventoryFileDAOTest {
 
     }
 
+    @Test
+    public void testUpdateProduct() throws IOException{
+        Product initialProduct = new Product(102, "Rock Water", 3.00F, 10);
+        inventoryFileDAO.createProduct(initialProduct); 
 
+        
+        Product updatedProduct = new Product(102, "Mineral Water", 1.50F, 150);
+
+        Product result = assertDoesNotThrow(() -> inventoryFileDAO.updateProduct(updatedProduct),
+            "Unexpected exception thrown");
+
+        Product notFound = new Product(0, "NotFound", 1.00f, 0);
+
+
+        assertNull(inventoryFileDAO.updateProduct(notFound));
+        assertNotNull(result);
+        assertEquals(updatedProduct.getId(), result.getId());
+        assertEquals(updatedProduct.getName(), result.getName());
+        assertEquals(updatedProduct.getQuantity(), result.getQuantity());
+        assertEquals(updatedProduct.getPrice(), result.getPrice());
+
+    }
 }
