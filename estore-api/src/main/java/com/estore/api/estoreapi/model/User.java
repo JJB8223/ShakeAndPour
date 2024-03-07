2e79b82c2cde03226655c41bd37949bf72494c8c
JJB8223 <jjb8223@g.rit.edu>
2024-03-06 23:46:08 -0500

    User Modifications

--- estore-api/src/main/java/com/estore/api/estoreapi/model/User.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/User.java
@@ -1,6 +1,7 @@
 package com.estore.api.estoreapi.model;
 
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.logging.Logger;
@@ -9,6 +10,7 @@ import java.util.logging.Logger;
  * Represents a User entity of the e-store
  *
  * @author Matthew Morrison msm8275
+ * @author Joshua Bay jjb8223
  */
 public class User {
     private static final Logger LOG = Logger.getLogger(User.class.getName());
@@ -20,21 +22,31 @@ public class User {
 
     @JsonProperty("name") private String name;
 
+    @JsonProperty("role") private UserRole role;
+
     /**
-     * Create a new user with a given id, username, password, and name
+     * Create a new user with a given id, username, password, name and role
      * @param id the id of the new User
      * @param username the username of the new User
      * @param password the password of the new User
-     * @param name the name of the new user
+     * @param name the name of the new User
+     * @param role the role of the new User
      */
     public User(@JsonProperty("id") int id,
                 @JsonProperty("username") String username,
                 @JsonProperty("password") String password,
-                @JsonProperty("name") String name){
+                @JsonProperty("name") String name,
+                @JsonProperty("role") UserRole role){
         this.id = id;
         this.username = username;
         this.password = password;
         this.name = name;
+        this.role = role;
+    }
+
+    public enum UserRole {
+        CUSTOMER,
+        ADMIN
     }
 
     public int getId(){return id;}
@@ -43,8 +55,12 @@ public class User {
 
     public void setUsername(String username) {this.username = username;}
 
+    public String getPassword() {return password;}
 
+    public void setPassword(String password) {this.password = password;}
 
+    public UserRole getRole() {return role;}
 
+    public void setRole(UserRole role) {this.role = role;}
 
 }
