6a02cda3ae7733814f075d99c3b35f1bdebf7b83
Akhil <devarapalli148@gmail.com>
2024-03-18 18:31:52 -0400

    Fixed all syntax issues

--- estore-ui/src/app/kits.service.ts
+++ estore-ui/src/app/kits.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { Kit } from './kit';
-import { Observable, catchError, tap, of} from 'rxjs';
 import { MessageService } from './message.service';
-
-import { HttpClient } from '@angular/common/http';
-
 
 @Injectable({
   providedIn: 'root'
 })
-export class KitsService {
+export class KitService {
+  private kitsUrl = 'http://localhost:8080/kits';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
-  private KitsUrl = 'http://localhost:8080/kits';
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) {}
 
-  constructor(private http: HttpClient,
-              private messageService: MessageService) { }
-
-
+  /**GET products from the server */
   getKits(): Observable<Kit[]> {
-    return this.http.get<Kit[]>(this.KitsUrl)
+    return this.http.get<Kit[]>(this.kitsUrl)
       .pipe(
-        tap(_=> this.log('Fetched Kits')),
+        tap(_ => this.log('fetched Kits')),
         catchError(this.handleError<Kit[]>('getKits', []))
       );
   }
@@ -31,7 +33,7 @@ export class KitsService {
       // if not search term, return empty Kit array.
       return of([]);
     }
-    return this.http.get<Kit[]>(`${this.KitsUrl}/?name=${term}`).pipe(
+    return this.http.get<Kit[]>(`${this.kitsUrl}/?name=${term}`).pipe(
       tap(x => x.length ?
         this.log(`found Kits matching "${term}"`) :
         this.log(`no Kits matching "${term}"`)),
@@ -39,33 +41,47 @@ export class KitsService {
     );
   }
 
-  // helper functions for the kits service class
-
-  private log(message: string) {
-    this.messageService.add(`ProductService: ${message}`);
+  /** POST: add a new Kit to the server */
+  addKit(kit: Kit): Observable<Kit> {
+    return this.http.post<Kit>(`${this.kitsUrl}/create`, kit, this.httpOptions).pipe(
+      tap((newKit: Kit) => this.log(`added Kit w/ id=${newKit.id}`)),
+      catchError(this.handleError<Kit>('addKit'))
+    );
   }
 
-    /**
-  * Handle Http operation that failed.
-  * Let the app continue.
-  *
-  * @param operation - name of the operation that failed
-  * @param result - optional value to return as the observable result
-  */
-    private handleError<T>(operation = 'operation', result?: T) {
-      return (error: any): Observable<T> => {
+  /** PUT: update the Kit on the server */
+  updateKit(kit: Kit): Observable<any> {
+    return this.http.put(this.kitsUrl, kit, this.httpOptions).pipe(
+      tap(_ => this.log(`updated Kit id=${kit.id}`)),
+      catchError(this.handleError<any>('updateKit'))
+    );
+  }
 
-        // TODO: send the error to remote logging infrastructure
-        console.error(error); // log to console instead
+  /**DELETE: delete the kit from the server */
+  deleteKit(id: number): Observable<Kit> {
+    const url = `${this.kitsUrl}/${id}`;
 
-        // TODO: better job of transforming error for user consumption
-        this.log(`${operation} failed: ${error.message}`);
+    return this.http.delete<Kit>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted Kit id=${id}`)),
+      catchError(this.handleError<Kit>('deleteKit'))
+    );
+  }
 
-        // Let the app keep running by returning an empty result.
-        return of(result as T);
-      };
-    }
+  private log(message: string) {
+    this.messageService.add(`KitService: ${message}`);
+  }
 
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
 
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
 
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
